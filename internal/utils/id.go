07b439beb73f799ae13e93e5530ff41a33e41a5a
Rom Rom <romdevmod@gmail.com>
2022-08-04 11:22:53 +0800

    Added `utils.GetMd5Sum()` that calculates the md5 sum of a resource URL.

--- internal/utils/id.go
+++ internal/utils/id.go
@@ -1,10 +1,15 @@
 package utils
 
 import (
+	"crypto/md5"
 	"crypto/sha512"
 	"fmt"
 )
 
 func getSha512Sum(resUrl string) string {
 	return fmt.Sprintf("%x", sha512.Sum512([]byte(resUrl)))
+}
+
+func GetMd5Sum(resUrl string) string {
+	return fmt.Sprintf("%x", md5.Sum([]byte(resUrl)))
 }
