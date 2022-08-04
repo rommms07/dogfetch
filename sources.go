80a4cee48b4bb47a49717723e097652707338b38
Rom Rom <romdevmod@gmail.com>
2022-08-04 22:09:21 +0800

    Moved `BreedInfo` def into `dogfetch.go`

--- sources.go
+++ sources.go
@@ -7,19 +7,3 @@ const (
 	Source2             = "http://www.infodogs.co.uk"
 	Source3             = "https://www.thekennelclub.org.uk"
 )
-
-type BreedInfo struct {
-	Id           string
-	Type         string
-	Name         string
-	Size         []string
-	Origin       []string
-	Colors       []string
-	Images       []string
-	Temperaments []string
-	OtherNames   []string
-	BreedGroups  []string
-	BreedChars   map[string]int
-	BreedRecs    []string
-	Refs         []string
-}
