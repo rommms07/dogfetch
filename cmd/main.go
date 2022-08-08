c86ebacd85c1f7116f2f1c9cd8619039ef1d25df
Rom Rom <romdevmod@gmail.com>
2022-08-08 19:01:10 +0800

    Added flags to `cmd/main.go`

--- cmd/main.go
+++ cmd/main.go
@@ -1,9 +1,35 @@
 package main
 
-import "github.com/rommms07/dogfetch"
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/rommms07/dogfetch"
+)
+
+var idParam = flag.String("id", "", "Get breed by id.")
+var nameParam = flag.String("name", "", "Get breed by name. (ex: ./cmd -name \"Golden Retriever\")")
+var allFlag = flag.Bool("all", false, "Get all dog breeds.")
 
 func main() {
-	alaskan := dogfetch.GetByName("Alaskan Malamute")
+	flag.Parse()
 
-	println(alaskan.Name)
+	var res any
+
+	if len(*idParam) != 0 {
+		res = dogfetch.GetById(*idParam)
+	} else if len(*nameParam) != 0 {
+		res = dogfetch.GetByName(*nameParam)
+	} else if *allFlag {
+		res = dogfetch.GetAll()
+	}
+
+	P, err := json.Marshal(res)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	fmt.Println(string(P))
 }
